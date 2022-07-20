1c31d351cb6e4c4b04f42e0f13f24cf23019b7dd
Strikeeaglechase <Strikeeaglechase@gmail.com>
2022-07-20 12:22:34 -0400

    Added IS_DEV config

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,6 @@
-export const API_URL = "https://api.vtolvr.live";
-export const WS_URL = "wss://alpha.vtolvr.live";
+export const IS_DEV = false;
+if (IS_DEV) console.warn(`-- Headless Client Development mode enable --`);
+
+export const API_URL = IS_DEV ? "http://localhost:8010" : "https://api.vtolvr.live";
+export const WS_URL = IS_DEV ? "ws://localhost" : "wss://alpha.vtolvr.live";
 export const IS_ALPHA = true;
