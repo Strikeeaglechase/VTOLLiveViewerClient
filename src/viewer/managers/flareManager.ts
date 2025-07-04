a3c855a7992ed62002188658785d071c79193a01
Strikeeaglechase <Strikeeaglechase@gmail.com>
2025-07-04 18:43:46 -0400

    Updated flare visuals

--- src/viewer/managers/flareManager.ts
+++ src/viewer/managers/flareManager.ts
@@ -5,9 +5,65 @@ import { Application } from "../app";
 import { Entity } from "../entityBase/entity";
 import { SceneManager } from "./sceneManager";
 
-const FLARE_LT = 5 * 1000;
-const FLARE_DRAG = 0.95;
-const FLARE_GRAV = -9;
+const ejectionSpeed = 45;
+const gravFactor = 0.55;
+const gravity = new Vector(0, gravFactor * -9.81, 0);
+const drag = 0.83;
+const life = 7000;
+const size = 1;
+
+const trailRate = 1000 / 30;
+const maxTrailLen = trailRate * 1.5;
+class FlareTrail {
+	private lineMesh: THREE.Line;
+	private lineGeom: THREE.BufferGeometry;
+	private linePoints: THREE.Vector3[] = [];
+	private lastTrailTime = 0;
+
+	private flare: Flare;
+
+	public color = {
+		r: 200,
+		g: 200,
+		b: 200
+	};
+
+	constructor() {}
+
+	public init(scene: SceneManager, flare: Flare) {
+		this.flare = flare;
+
+		const lineMat = new THREE.LineBasicMaterial({ color: new THREE.Color(this.color.r, this.color.g, this.color.b) });
+		this.lineGeom = new THREE.BufferGeometry();
+		this.lineMesh = new THREE.Line(this.lineGeom, lineMat);
+		this.lineMesh.frustumCulled = false;
+		this.lineMesh.name = "Chunked unit trail line";
+
+		scene.add(this.lineMesh);
+	}
+
+	public update(dt: number) {
+		if (Application.time - this.lastTrailTime > trailRate) {
+			this.lastTrailTime = Application.time;
+			this.linePoints.push(new THREE.Vector3(this.flare.position.x, this.flare.position.y, this.flare.position.z));
+
+			if (!this.lineMesh.visible) {
+				this.lineMesh.visible = true;
+			}
+		}
+
+		if (this.linePoints.length > maxTrailLen) this.linePoints.shift();
+
+		this.linePoints[this.linePoints.length - 1]?.set(this.flare.position.x, this.flare.position.y, this.flare.position.z);
+		this.lineGeom.setFromPoints(this.linePoints);
+	}
+
+	public clear() {
+		this.linePoints = [];
+		this.lineGeom.setFromPoints(this.linePoints);
+		this.lineMesh.visible = false;
+	}
+}
 
 interface Flare {
 	position: Vector;
@@ -15,61 +71,53 @@ interface Flare {
 	createdAt: number;
 	mesh: THREE.Mesh;
 	isAlive: boolean;
+	trail: FlareTrail;
 }
 
 class FlareManager {
 	private flares: Flare[] = [];
 	private freeFlares: Flare[] = [];
-	private scaleMult = 85;
+	private scaleMult = 1;
 
-	private geometry: THREE.SphereGeometry;
+	private geometry: THREE.BoxGeometry;
 	private material: THREE.MeshStandardMaterial;
 
 	constructor(private scene: SceneManager) {
-		this.geometry = new THREE.SphereGeometry(1.5, 6, 6);
+		// this.geometry = new THREE.SphereGeometry(1.5, 6, 6);
+		this.geometry = new THREE.BoxGeometry(size, size, size, 1, 1, 1);
 		this.material = new THREE.MeshStandardMaterial({ color: "#000000", side: THREE.DoubleSide });
 		this.material.emissiveIntensity = 1;
-		this.material.emissive = new THREE.Color("#ffe357");
+		// this.material.emissive = new THREE.Color("#ffe357");
+		this.material.emissive = new THREE.Color("#fff25e");
 	}
 
 	public update(dt: number) {
 		const now = Application.time;
 		this.flares.forEach(flare => {
 			if (!flare.isAlive) return;
+			this.updateFlare(flare, dt);
 
-			// TODO: Make flares move a bit to match vtol better
-			flare.velocity = flare.velocity.multiply(FLARE_DRAG).add(new Vector(0, (FLARE_GRAV * dt) / 1000, 0));
-			flare.position = flare.position.add(flare.velocity.multiply(dt / 1000));
-			flare.mesh.position.set(flare.position.x, flare.position.y, flare.position.z);
-
-			const timeAlive = now - flare.createdAt;
-
-			const amountThrough = 1 - timeAlive / FLARE_LT;
-
-			// Make flares still half size when fully faded
-			const amountThroughAdjusted = 1 - (timeAlive / FLARE_LT) * 0.8;
-
-			flare.mesh.scale.set(
-				1.5 * amountThroughAdjusted * this.scaleMult,
-				1.5 * amountThroughAdjusted * this.scaleMult,
-				1.5 * amountThroughAdjusted * this.scaleMult
-			);
-
-			// It always is, this is just to make TS happy
-			if (flare.mesh.material instanceof THREE.MeshStandardMaterial) {
-				flare.mesh.material.transparent = true;
-				flare.mesh.material.opacity = amountThrough;
-			}
-
-			if (flare.createdAt + FLARE_LT < now || now < flare.createdAt) {
+			if (flare.createdAt + life < now || now < flare.createdAt) {
 				flare.mesh.visible = false;
 				flare.isAlive = false;
-				flare.mesh.scale.set(2, 2, 2);
+				flare.trail.clear();
+				// flare.mesh.scale.set(2, 2, 2);
 				this.freeFlares.push(flare);
 			}
 		});
+	}
 
-		// this.flares = this.flares.filter(f => f.isAlive);
+	private updateFlare(flare: Flare, dt: number) {
+		const gravForce = gravity.clone().multiply(dt / 1000);
+		const dragForce = flare.velocity.multiply(-drag).multiply(dt / 1000);
+		flare.velocity = flare.velocity.add(gravForce).add(dragForce);
+		flare.position = flare.position.add(flare.velocity.multiply(dt / 1000));
+
+		flare.mesh.position.set(flare.position.x, flare.position.y, flare.position.z);
+
+		flare.trail.update(dt);
+
+		flare.mesh.scale.set(this.scaleMult, this.scaleMult, this.scaleMult);
 	}
 
 	public fireCm(entity: Entity) {
@@ -81,28 +129,61 @@ class FlareManager {
 			mesh.name = "Flare";
 			this.scene.add(mesh);
 
-			this.flares.push({
+			const flare: Flare = {
 				position: entity.position.clone(),
-				velocity: entity.velocity.clone().multiply(0.99),
+				velocity: this.getFlareInitVelocity(entity),
 				createdAt: Application.time,
 				isAlive: true,
-				mesh: mesh
-			});
+				mesh: mesh,
+				trail: new FlareTrail()
+			};
+
+			flare.trail.init(this.scene, flare);
+
+			this.flares.push(flare);
 		} else {
 			const flare = this.freeFlares.pop();
 			if (!flare) throw new Error("FlareManager: flare is null");
 			flare.position = entity.position.clone();
-			flare.velocity = entity.velocity.clone().multiply(0.99);
+			flare.velocity = this.getFlareInitVelocity(entity);
 			flare.createdAt = Application.time;
 			flare.isAlive = true;
 			flare.mesh.visible = true;
 			flare.mesh.position.set(flare.position.x, flare.position.y, flare.position.z);
-			this.flares.push(flare);
+			// this.flares.push(flare);
 		}
 	}
 
+	private getFlareInitVelocity(entity: Entity) {
+		const num = Math.random();
+		const maxAngle = 3;
+		const maxRadDelta = maxAngle * (num * num) * (Math.PI / 180);
+
+		const rotation = new THREE.Euler(entity.rotation.x, entity.rotation.y, entity.rotation.z);
+		const ejectDir = new THREE.Vector3(0, -1, 0).applyQuaternion(new THREE.Quaternion().setFromEuler(rotation));
+		const randOffset = new THREE.Vector3().randomDirection().projectOnPlane(ejectDir);
+
+		const angle = ejectDir.angleTo(randOffset);
+		const rotationAxis = new THREE.Vector3().crossVectors(ejectDir, randOffset).normalize();
+		const clampedAngle = Math.min(angle, maxRadDelta);
+
+		ejectDir.applyAxisAngle(rotationAxis, clampedAngle);
+
+		const eSpeedLow = ejectionSpeed * 0.9;
+		const eSpeedHigh = ejectionSpeed * 1.1;
+		const ejectSpeed = eSpeedLow + Math.random() * (eSpeedHigh - eSpeedLow);
+		const velocity = new Vector(
+			entity.velocity.x + ejectDir.x * ejectSpeed,
+			entity.velocity.y + ejectDir.y * ejectSpeed,
+			entity.velocity.z + ejectDir.z * ejectSpeed
+		);
+
+		return velocity;
+	}
+
 	public updateScale(scale: number) {
-		this.scaleMult = scale * 0.9;
+		console.log(scale);
+		this.scaleMult = Math.max(1, scale * 0.7);
 	}
 }
 
